08f8bdec7c66910551d5a94fcbf1ff2711ca87d8
Robin Schmid <Robin Schmid@192.168.0.59>
2018-05-20 14:59:30 +0200

    ungetestet: kleinsten Kreis bei 2 Punkten finden

--- src/kleinster/kreis/KleinsterKreis.java
+++ src/kleinster/kreis/KleinsterKreis.java
@@ -19,9 +19,9 @@ public class KleinsterKreis {
 
         int[] ausgelosteP = new int[auszulosendeP]; //Index in points
         Tuple[] points = new Tuple[anzahlP];        //Koordinaten
-        Tuple cCenter;  //Zentrum des Kreises
+        Tuple cCenter = new Tuple(0, 0);  //Zentrum des Kreises
 
-        float radius;
+        double radius = 0;
 
         //pos. auslosen
         for (int i = 0; i < anzahlP; i++) {
@@ -44,7 +44,7 @@ public class KleinsterKreis {
                 System.out.println("Wkeit von Punkt " + i + " : " + wkeiten[i]);
             }
 
-            //zu überprüfende Punkte auslosen
+            //zu überprüfende Punkte auslosen. getestet
             for (int indexLP = 0; indexLP < auszulosendeP; indexLP++) {
                 float zz = (float) Math.random();
                 System.out.println("Zufallszahl: " + zz);
@@ -74,7 +74,7 @@ public class KleinsterKreis {
                         if (ausgelosteP[i] == ausgelosteP[indexLP]) {
                             equal = true;
                             zz = (float) Math.random();
-                            System.out.println("Gleiche Zahl! Neue Zufallszahl generiert: "+zz);
+                            System.out.println("Gleiche Zahl! Neue Zufallszahl generiert: " + zz);
                             break;
                         } else {
                             equal = false;
@@ -85,12 +85,68 @@ public class KleinsterKreis {
                     }
                 }
             }
+            boolean foundCircle = false;
+            //Alle möglichen Paare von Punkten checken und schauen ob sie den Kleinste Kreis definieren. Nicht getestet 
             for (int i = 0; i < auszulosendeP; i++) {
-                System.out.println("Gesammtindex von Punkt " + i + ": " + ausgelosteP[i]);
+                for (int j = 0; j < auszulosendeP; j++) {
+                    int x1 = points[ausgelosteP[i]].x;
+                    int x2 = points[ausgelosteP[j]].x;
+
+                    int y1 = points[ausgelosteP[i]].y;
+                    int y2 = points[ausgelosteP[j]].y;
+
+                    int dx = Math.abs(x1 - x2);
+                    int dy = Math.abs(y1 - y2);
+
+                    cCenter.x = (x1 + x2) / 2;
+                    cCenter.y = (y1 + y2) / 2;
+
+                    radius = Math.pow((dx * dx + dy * dy), 0.5);
+
+                    //checken ob alle drin sind
+                    foundCircle = true;
+                    for (int k = 0; k < auszulosendeP; k++) {
+                        int x = points[ausgelosteP[k]].x;
+                        int y = points[ausgelosteP[k]].y;
+
+                        double dx2 = Math.abs(cCenter.x - x);
+                        double dy2 = Math.abs(cCenter.y - y);
+
+                        if (radius < Math.pow((dx2 * dx2 + dy2 * dy2), 0.5)) {//wenn der Kreis nicht der kleinste umschließende ist check beenden
+                            foundCircle = false;
+                            break;
+                        }
+                    }
+                    if (foundCircle) {
+                        break;
+                    }
+                }
+                if (foundCircle) {
+                    break;
+                }
             }
+
+            //wenn kein Kreis gefunden wurde, alle Kreise, die durch drei Punkte definiert werden checken
+            if (!foundCircle) {
+                System.out.println("3er Suche");
+            }
+
+            //checken ob alle drin sind
             finished = true;
+            for (int k = 0; k < anzahlP; k++) {
+                int x = points[k].x;
+                int y = points[k].y;
+
+                double dx2 = Math.abs(cCenter.x - x);
+                double dy2 = Math.abs(cCenter.y - y);
+
+                if (radius < Math.pow((dx2 * dx2 + dy2 * dy2), 0.5)) {//wenn der Kreis nicht der kleinste umschließende ist check beenden
+                    lose[k] *= 2;
+                    finished = false;
+                }
+            }
         }
-
+        System.out.println("Gebrauchte Zeit: " + (System.currentTimeMillis() - startTime) / 1000 + "s");
+        System.out.println("fertig");
     }
-
 }
